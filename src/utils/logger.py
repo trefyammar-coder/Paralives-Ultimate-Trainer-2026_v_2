d4cfd8d0876668352ec3e61d72c772529fea7d4a
we9lii <wesli808@gmail.com>
2026-06-22 18:42:58 +0000

    Create src/utils/logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,15 @@
+
+from colorama import init, Fore
+init(autoreset=True)
+
+def log_info(msg):
+    print(f"{Fore.CYAN}[INFO] {msg}")
+
+def log_success(msg):
+    print(f"{Fore.GREEN}[SUCCESS] {msg}")
+
+def log_warning(msg):
+    print(f"{Fore.YELLOW}[WARN] {msg}")
+
+def log_error(msg):
+    print(f"{Fore.RED}[ERROR] {msg}")
